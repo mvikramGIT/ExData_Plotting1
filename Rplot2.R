6f4133984e5e78d8f6753fa47ffc030d9aa0bf57
Vikram Mukundan <Vikram@vikram-mukundans-macbook-pro-2.local>
2014-12-07 14:52:26 -0800

    Plot2

--- Rplot2.R
+++ Rplot2.R
@@ -1,7 +1,12 @@
-#dt <- read.table("household_power_consumption.txt", header = TRUE, sep =";", stringsAsFactors = FALSE)
-#dt$Date <- as.Date(dt$Date, "%d/%m/%Y")
-#dt <- dt[(dt$Date == "2007-02-01"|dt$Date == "2007-02-02"),]
-#dt$Time <- strptime(paste(dt$Date,dt$Time), "%Y-%m-%d %H:%M:%S")
+# Runs with the file househole_power_consumption.txt in the working folder
+# file downloaded from :
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+dt <- read.table("household_power_consumption.txt", header = TRUE, sep =";", stringsAsFactors = FALSE)
+dt$Date <- as.Date(dt$Date, "%d/%m/%Y") # Parse dates
+dt <- dt[(dt$Date == "2007-02-01"|dt$Date == "2007-02-02"),] # Selected two days from dataset
+dt$Time <- strptime(paste(dt$Date,dt$Time), "%Y-%m-%d %H:%M:%S") # Parse datetime
 with(dt,
      plot(Time, Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab ="")
     )
+dev.copy(png, file = "plot2.png",width = 480, height = 480)
+dev.off()
