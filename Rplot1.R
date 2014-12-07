e7c7ee67eff61bb6e83e1f6e9280ae421a9c6bf4
Vikram Mukundan <Vikram@vikram-mukundans-macbook-pro-2.local>
2014-12-07 14:51:53 -0800

    Updated Rplot1.r

--- Rplot1.R
+++ Rplot1.R
@@ -1,7 +1,10 @@
-#dt <- read.table("household_power_consumption.txt", header = TRUE, sep =";", stringsAsFactors = FALSE)
-#dt$Date <- as.Date(dt$Date, "%d/%m/%Y")
-#dt <- dt[(dt$Date == "2007-02-01"|dt$Date == "2007-02-02"),]
-#dt$Time <- strptime(paste(dt$Date,dt$Time), "%Y-%m-%d %H:%M:%S")
+# Runs with the file househole_power_consumption.txt in the working folder
+# file downloaded from :
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+dt <- read.table("household_power_consumption.txt", header = TRUE, sep =";", stringsAsFactors = FALSE)
+dt$Date <- as.Date(dt$Date, "%d/%m/%Y") # Parse dates
+dt <- dt[(dt$Date == "2007-02-01"|dt$Date == "2007-02-02"),] # Selected two days from dataset
+dt$Time <- strptime(paste(dt$Date,dt$Time), "%Y-%m-%d %H:%M:%S") # Parse datetime
 hist(as.numeric(dt$Global_active_power),
      main ="Global Active Power",
      xlab = "Global Active Power (kilowatts)",
@@ -10,3 +13,5 @@ hist(as.numeric(dt$Global_active_power),
      xaxt ="n"
      )
 axis(1,at=c(0,2,4,6))
+dev.copy(png,file = "plot1.png",width = 480, height = 480)
+dev.off()
